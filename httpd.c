48d7db1e633fb4c8995823cd6f2f693fc02aa721
namju <kimnamju0920@gmail.com>
2024-09-12 04:56:30 +0000

    run argument option

--- httpd.c
+++ httpd.c
@@ -12,6 +12,20 @@
 #include <ctype.h>
 #include <signal.h>
 
+#define USAGE "Usage: %s [--port=n] [--chroot --user=u --group=g] <docroot>\n"
+
+static int debug_mode = 0;
+
+static struct option longopts[] = {
+	{"debug", no_argument, &debug_mode, 1},
+	{"chroot", no_argument, NULL, 'c'},
+	{"user", required_argument, NULL, 'u'},
+	{"group", required_argument, NULL, 'g'},
+	{"port", required_argument, NULL, 'p'},
+	{"help", no_argument, NULL, 'h'},
+	{0, 0, 0, 0}
+};
+
 /* -- constants -- */
 #define SERVER_NAME "httpd"
 #define SERVER_VERSION "0.1"
@@ -73,24 +87,56 @@ static char* guess_content_type(struct FileInfo* info);
 
 /* -- main -- */
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s <docroot>\n", argv[0]);
-		exit(1);
-	}
-    
-	struct stat sb;
-	if (stat(argv[1], &sb) == -1) {
-		perror("stat");
-		exit(1);
-	}
-	
-	if (!(S_ISDIR(sb.st_mode))) {
-		fprintf(stderr, "%s is not a directory\n", argv[1]);
-		exit(1);
+	int server_fd;
+	char *port = NULL;
+	char *docroot;
+	int do_chroot = 0;
+	char *user = NULL;
+	char *group = NULL;
+	int opt;
+
+	while ((opt = getopt_long(argc, argv, "", longopts, NULL)) != -1) {
+		switch(opt) {
+		case 0:
+			break;
+		case 'c':
+			do_chroot = 1;
+			break;
+		case 'u':
+			user = optarg;
+			break;
+		case 'g':
+			group = optarg;
+			break;
+		case 'p':
+			port = optarg;
+			break;
+		case 'h':
+			fprintf(stdout, USAGE, argv[0]);
+			exit(0);
+		case '?':
+			fprintf(stderr, USAGE, argv[0]);
+			exit(1);
+		}
 	}
 
+	if (optind != argc - 1) {
+		fprintf(stderr, USAGE, argv[0]);
+		exit(1);
+	}
+	docroot = argv[optind];
+	if (do_chroot) {
+		setup_environment(docroot, user, group);
+		docroot="";
+	}
 	install_signal_handlers();
-	service(stdin, stdout, argv[1]);
+	server_fd = listen_socket(port);
+	if (!debug_mode) {
+		openlog(SERVER_NAME, LOG_PID|LOG_NDELAY, LOG_DAEMON);
+		become_daemon();
+	}
+
+	server_main(server_fd, docroot);
 	exit(0);
 }
 
